bf292b24d6ec903976a38094a89856cb333678b5
tianqyaq <tqytianqingyi@163.com>
2024-11-15 13:32:55 +0800

    Signed-off-by: tianqyaq <tqytianqingyi@163.com>

---
+++ js/bookImgData.js
@@ -0,0 +1,132 @@
+// 书本目录，如果无ols变量则无目录，ols 变量名勿删，全局共享
+var ols = [
+  {
+    "caption": "1",
+    "page": "1",
+  },
+  {
+    "caption": "2",
+    "page": "2",
+  },
+  {
+    "caption": "3",
+    "page": "3",
+  },
+  {
+    "caption": "4",
+    "page": "4",
+  },
+  {
+    "caption": "5",
+    "page": "5",
+  },
+  {
+    "caption": "6",
+    "page": "6",
+  },
+  {
+    "caption": "7",
+    "page": "7",
+  },
+  {
+    "caption": "8",
+    "page": "8",
+  },
+  {
+    "caption": "9",
+    "page": "9",
+  },
+  {
+    "caption": "10",
+    "page": "10",
+  },
+  {
+    "caption": "11",
+    "page": "11",
+  },
+  {
+    "caption": "12",
+    "page": "12",
+  },
+  {
+    "caption": "13",
+    "page": "13",
+  },
+  {
+    "caption": "14",
+    "page": "14",
+  },
+  {
+    "caption": "15",
+    "page": "15",
+  },
+  {
+    "caption": "16",
+    "page": "16",
+  },
+  {
+    "caption": "17",
+    "page": "17",
+  },
+  {
+    "caption": "18",
+    "page": "18",
+  },
+  {
+    "caption": "19",
+    "page": "19",
+  },
+  {
+    "caption": "20",
+    "page": "20",
+  },
+  {
+    "caption": "21",
+    "page": "21",
+  },
+  {
+    "caption": "22",
+    "page": "22",
+  },
+  {
+    "caption": "23",
+    "page": "23",
+  },
+  {
+    "caption": "24",
+    "page": "24",
+  },
+  {
+    "caption": "25",
+    "page": "25",
+  },
+  {
+    "caption": "26",
+    "page": "26",
+  },
+  {
+    "caption": "27",
+    "page": "27",
+  },
+  {
+    "caption": "28",
+    "page": "28",
+  },
+  {
+    "caption": "29",
+    "page": "29",
+  },
+  {
+    "caption": "30",
+    "page": "30",
+  },
+  
+];
+
+// 路径配置
+var loadImgpath = "./files/thumb/"
+
+bookConfig.largePath = loadImgpath    // 大图路径
+bookConfig.normalPath = loadImgpath
+bookConfig.thumbPath = loadImgpath
+bookConfig.totalPageCount = ols.length    // 页面数量
